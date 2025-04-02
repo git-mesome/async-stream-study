4dd6f53b27bc6bdd1a6d54d71250c3e1019108a3
Minseo Kim <kms12782@gmail.com>
2025-04-02 20:58:26 +0900

    chore: initialize sse-flux-demo project with essential configurations and structure

---
+++ sse-flux-demo/src/main/java/io/wisoft/sseflux/SseFluxDemoApplication.java
@@ -0,0 +1,13 @@
+package io.wisoft.sseflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SseFluxDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SseFluxDemoApplication.class, args);
+	}
+
+}
